66d40724cb9c7ace135421857045fd6405907262
Abraham <abraham.barros@situm.es>
2018-02-12 13:31:22 +0100

    update to sdk version 2.5.2

--- GettingStarted/SitumSDK.framework/Versions/A/Headers/SITAngle.h
+++ GettingStarted/SitumSDK.framework/Versions/A/Headers/SITAngle.h
@@ -18,4 +18,7 @@
 
 - (float)radians;
 
+- (float)degressClockwise;
+
+- (float)radiansMinusPiPi;
 @end
--- GettingStarted/SitumSDK.framework/Versions/A/Headers/SitumSDK.h
+++ GettingStarted/SitumSDK.framework/Versions/A/Headers/SitumSDK.h
@@ -9,6 +9,9 @@
 #ifndef SitumSDK_h
 #define SitumSDK_h
 
+#ifndef PublicSDK_h
+#define PublicSDK_h
+
 #import <Foundation/Foundation.h>
 
 #import "SITBuildingInfo.h"
@@ -47,7 +50,6 @@
 #import "SITIndoorRouteStep.h"
 
 #import "SITDiagnosisServices.h"
-#import "SITCalibrationManager.h"
 
 #import "SITOccurrence.h"
 #import "SITKeyValueFilter.h"
@@ -90,11 +92,10 @@
 
 #import "SITBounds.h"
 
-
-#import "SITCalibrationManager.h"
+#ifndef PublicSDK_h
 #import "SITBeaconManager.h"
-#import "SITNode.h"
-#import "SITLink.h"
+#import "SITCalibrationManager.h"
+#endif
 
 /*!
  *  The Situm Framework (SitumSDK.framework) provides information about the buildings supporting indoor location services as well as location data of a user inside a building. The framework uses a multi-sensor approach to find the user's location inside a building.
@@ -142,5 +143,5 @@
 @interface SitumSDK : NSObject
 
 @end
-
+#endif
 #endif
