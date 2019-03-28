2d2414bc4120dbb265491a04fefbcb8238f15eb5
Fernando Sanchez Vilas <fsvilas@gmail.com>
2019-03-28 14:46:33 +0100

    Positioning Sample

--- GettingStarted/src/Samples/Positioning/SGSPositioningVC.m
+++ GettingStarted/src/Samples/Positioning/SGSPositioningVC.m
@@ -9,8 +9,16 @@
 #import "SGSPositioningVC.h"
 @import CoreLocation;
 
-@interface SGSPositioningVC ()
+@interface SGSPositioningVC () <SITLocationDelegate>
+
+@property (weak, nonatomic) IBOutlet UIButton *startPositioningButton;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UILabel *positionLabel;
+
 @property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic) BOOL isPositioning;
+@property (nonatomic, strong) NSDictionary *locationStatesTranslator;
+
 @end
 
 // Request user permission strings
@@ -22,6 +30,16 @@ static NSString *PermissionDeniedOk = @"Ok";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self customizeNavigationBarWithTitle:@"Positioning"];
+    self.isPositioning = NO;
+    self.locationStatesTranslator = @{@(0):@"Stopped",
+                                  @(1):@"Calculating",
+                                  @(2):@"Not In Building",
+                                  @(3):@"Started",
+                                  @(4):@"Needs Calibration"
+                                  };
+    self.locationManager = [[CLLocationManager alloc] init];
+    [SITLocationManager sharedInstance].delegate = self;
 }
 
 -(void)viewDidAppear:(BOOL)animated{
@@ -30,6 +48,12 @@ static NSString *PermissionDeniedOk = @"Ok";
     [self requestLocationAuthorization];
 }
 
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    [self stopPositioning];
+}
+
+#pragma mark -- Request Location Authorization
 -(void)requestLocationAuthorization{
     switch ([CLLocationManager authorizationStatus]) {
         case kCLAuthorizationStatusNotDetermined:{
@@ -57,4 +81,63 @@ static NSString *PermissionDeniedOk = @"Ok";
     }
 }
 
+- (IBAction)startPositioningButtonPressed:(id)sender {
+    if (self.isPositioning){
+        [self stopPositioning];
+    }else{
+        [self startPositioning];
+    }
+}
+
+#pragma mark - Start/Stop Positioning
+-(void)startPositioning{
+    SITLocationRequest *request = [[SITLocationRequest alloc] initWithBuildingId:self.selectedBuildingInfo.building.identifier];
+    // Configure here custom beacons if neccessary (through property beaconFilters of request object).
+    [[SITLocationManager sharedInstance] requestLocationUpdates:request];
+    self.isPositioning = YES;
+}
+
+-(void)stopPositioning{
+    [[SITLocationManager sharedInstance] removeUpdates];
+    self.isPositioning = NO;
+}
+
+#pragma mark - SITLocationDelegate Methods
+
+- (void)locationManager:(id<SITLocationInterface>)locationManager
+         didUpdateState:(SITLocationState)state
+{
+    self.statusLabel.text = self.locationStatesTranslator[@(state)];
+    NSLog(@"location manager changed to state: %d", state);
+    if (state == kSITLocationUserNotInBuilding) {
+        NSLog(@"Unable to find user on building. Please verify this building has been configured (calibrated) correctly");
+    }
+}
+
+- (void)locationManager:(id<SITLocationInterface>)locationManager
+       didFailWithError:(NSError *)error
+{
+    NSLog(@"An error happened: %@", error);
+}
+
+- (void)locationManager:(id<SITLocationInterface>)locationManager
+      didUpdateLocation:(SITLocation *)location
+{
+    NSLog(@"New location update available: :%@", location);
+            self.statusLabel.text = @"";
+    self.positionLabel.text = [NSString stringWithFormat:@" building: %@\n floor: %@\n x:%f\n y:%f\n yaw:%@ radians\n accuracy:%f\n", location.position.buildingIdentifier, location.position.floorIdentifier,location.position.cartesianCoordinate.x, location.position.cartesianCoordinate.y,location.bearing, location.accuracy];
+}
+
+#pragma mark -- is positioning setter / Screen state customization
+-(void)setIsPositioning:(BOOL)isPositioning{
+    _isPositioning=isPositioning;
+    //Change positioning title
+    NSString *positioningButtonTitle = isPositioning?@"Stop Positioning":@"Start Positioning";
+    [self.startPositioningButton setTitle:positioningButtonTitle forState:UIControlStateNormal];
+    //If no positioning change label text
+    if(!isPositioning){
+        self.statusLabel.text = @"";
+        self.positionLabel.text = @"";
+    }
+}
 @end
