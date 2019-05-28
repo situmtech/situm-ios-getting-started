aadde244b2b510b5f78c9256d5df0337caa5e79f
Fernando Sanchez Vilas <fsvilas@gmail.com>
2019-05-28 19:26:39 +0200

    Set drawBuilding button in navigation bar

--- GettingStarted/src/Samples/DrawBuilding/README.md
+++ GettingStarted/src/Samples/DrawBuilding/README.md
@@ -5,7 +5,7 @@ Another interesting functionality is to show the floorplan of a building on top 
 
 As a required step, you will need to complete the steps in the [Setup Google Maps section](https://github.com/situmtech/situm-ios-getting-started#mapsapikey). Once this is done, there are 5 steps your app has to follow in order to draw a floorplan image on a GoogleMaps map.
 
--  Add the map to your view. You can find more information in the section [Getting Started](https://developers.google.com/maps/documentation/ios-sdk/start) of Google Maps documentation. If, as in the example app, you want to show the map in a subview of your controller, make sure to set your view class as `GMSMapView` in the storyboard and that you assign the map to your view as done in our sample code.
+-  Add the map to your view. You can find more information in the section [Getting Started](https://developers.google.com/maps/documentation/ios-sdk/start) of Google Maps documentation.
 
 - Animate the camera to the coordinates where your building is located.
 
--- GettingStarted/src/Samples/DrawBuilding/SGSDrawBuildingVC.m
+++ GettingStarted/src/Samples/DrawBuilding/SGSDrawBuildingVC.m
@@ -10,51 +10,50 @@
 @import GoogleMaps;
 
 @interface SGSDrawBuildingVC ()
-//Make sure this view is of class GMSMapView in storyboard, otherwise the map wouldnt appear
-@property (weak, nonatomic) IBOutlet GMSMapView *mapView;
+@property (nonatomic,strong) GMSMapView *googleMapView;
 @end
 
 static NSString *NoFloorsInBuildingAlertTitle = @"No Floors";
 static NSString *NoFloorsInBuildingAlertBody = @"At least one floor is needed to draw the building.";
 static NSString *NoFloorsInBuildingOk = @"Ok";
 
+static NSString *DrawBuildingButtonText = @"Draw Building";
+
 @implementation SGSDrawBuildingVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-}
-
--(void)viewWillAppear:(BOOL)animated{
-    [super viewWillAppear:animated];
     [self drawGoogleMap];
+    [self addDrawBuildingButton];
 }
 
 #pragma mark - Draw Google Map in Proper View
 -(void)drawGoogleMap{
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86 longitude:151.20 zoom:6];
-    GMSMapView *mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-    //Use addSubview instead of self.mapView=mapView
-    //As you are not presenting the map in self.view but in a self.view subview, use [self.mapView addSubview:mapView]; instead of self.mapView=mapView;
-    [self.mapView addSubview:mapView];
+    self.googleMapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    self.view=self.googleMapView;
+}
+
+-(void)addDrawBuildingButton{
+    UIBarButtonItem *drawBuildingButton = [[UIBarButtonItem alloc] initWithTitle:DrawBuildingButtonText style:UIBarButtonItemStylePlain target:self
+                                                                          action:@selector(drawBuilding)];
+    self.navigationItem.rightBarButtonItem=drawBuildingButton;
 }
 
 #pragma mark - Draw Building in Map
-- (IBAction)drawBuilding:(id)sender {
+- (void)drawBuilding{
     //Move camera to building coordinates and set the desired zoom
     GMSCameraPosition *cameraPosition = [GMSCameraPosition cameraWithTarget:self.selectedBuildingInfo.building.center zoom:19];
-    [(GMSMapView *)self.mapView animateToCameraPosition:cameraPosition];
-    
+    [self.googleMapView animateToCameraPosition:cameraPosition];
     //Select the floor to draw
        SITFloor *selectedFloor = self.selectedBuildingInfo.floors[0];
-    
     //Get the floor image
-    
     [[SITCommunicationManager sharedManager] fetchMapFromFloor:selectedFloor
                                                 withCompletion:^(NSData *imageData) {
                                                     //Create Google Maps overly from Situm data
                                                     GMSGroundOverlay *overlay = [self createGMSGroundOverlyImage:imageData bounds:self.selectedBuildingInfo.building.bounds bearing:self.selectedBuildingInfo.building.rotation.degrees];
                                                     //Assign our map as the overly map
-                                                    overlay.map=self.mapView;
+                                                    overlay.map=self.googleMapView;
                                                 }];
     
 }
@@ -66,5 +65,7 @@ static NSString *NoFloorsInBuildingOk = @"Ok";
     mapOverlay.bearing = bearing;
     return mapOverlay;
 }
+
+
 
 @end
